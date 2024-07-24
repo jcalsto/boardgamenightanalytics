6347aa1c266f4eee0866dcec9c99fa91c160a8f6
Jorrel Sto Tomas <61225248+jcalsto@users.noreply.github.com>
2024-07-24 14:08:01 -0700

    added most frequent attender metric

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,6 +35,9 @@ def clear_select():
     st.session_state.input_name = ''
     st.session_state.selected_name = st.session_state.dropdown_name
 
+attendance_count = guest_df[guest_df['Status'] == 'Going'].groupby('Name').size().reset_index(name='Attendance Count')
+max_attendance = attendance_count[attendance_count['Attendance Count'] == attendance_count['Attendance Count'].max()]
+
 
 # -----------------------------------------------------------------------------
 # Draw the actual page
@@ -46,6 +49,12 @@ def clear_select():
 All data is exported from the guest lists from my [Partiful Invites](https://www.partiful.com/). The data in this,
 just reflects up until board game night that was on July 18. This dashboard will evolve as I add more filtering and inputs.
 '''
+
+st.subheader('Fun General Event Metrics')
+
+st.write('Person(s) who attended the most events:')
+st.dataframe(max_attendance)
+
 
 # Add some spacing
 ''
