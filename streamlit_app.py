33f0fe64717dab4d5f9400d7a701b267ab8c0627
Jorrel Sto Tomas <61225248+jcalsto@users.noreply.github.com>
2024-07-31 01:38:47 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,7 @@ attendance_df = pd.merge(attendance_df, maybe_count, on='Name', how='left').fill
 attendance_df['Going Ratio'] = attendance_df['Going Count'] / attendance_df['Total Invites']
 
 # Filter out attendees with fewer than 3 total invites
-attendance_df = attendance_df[attendance_df['Total Invites'] > 4]
+attendance_df = attendance_df[attendance_df['Total Invites'] > 2]
 
 # Exclude "Jorrel Sto Tomas" for top 5 ratio calculation
 filtered_attendance_df = attendance_df[attendance_df['Name'] != 'Jorrel Sto Tomas']
